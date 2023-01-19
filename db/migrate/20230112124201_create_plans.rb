759f119e65fa32d9396bac2256b2d222b31db001
HoneyTgs <112701355+HoneyTgs@users.noreply.github.com>
2023-01-19 21:33:59 +0900

    commit

--- db/migrate/20230112124201_create_plans.rb
+++ db/migrate/20230112124201_create_plans.rb
@@ -1,7 +1,10 @@
 class CreatePlans < ActiveRecord::Migration[6.0]
   def change
     create_table :plans do |t|
-      
+      t.references :user,        null: false, foreign_key: true
+      t.string     :title,       null: false
+      t.string     :detail,      null: false
+      t.integer    :category_id, null: false
       t.timestamps
     end
   end
